b33ae2a200011fbe9fb94030fdde22ae0bca146e
Julien-ROBERT LECADOU <lecadou@gmail.com>
2025-04-15 14:54:56 -0400

    Update assembly informational version and cache inputs

--- MyNet8App/obj/Debug/net8.0/MyNet8App.AssemblyInfo.cs
+++ MyNet8App/obj/Debug/net8.0/MyNet8App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNet8App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145d4ac0cab1405944c9b4c8bb729404067347e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb59041d4fdbe5d2b503163c83ad68d1f23d4b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNet8App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNet8App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
